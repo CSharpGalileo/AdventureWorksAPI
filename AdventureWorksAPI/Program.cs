e380f8f49f8a4766c53e986212e95af2d4648c4e
Abel Palma <apalma@tsc.com.gt>
2022-09-07 23:04:21 -0400

    Agregar otro controlador y opciones de header

--- AdventureWorksAPI/Program.cs
+++ AdventureWorksAPI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 using AdventureWorksNS.Data;
 using static System.Console;
 using AdventureWorksAPI.Repositories;
+using System.Text.Json.Serialization;
 
 namespace AdventureWorksAPI
 {
@@ -10,6 +11,7 @@ namespace AdventureWorksAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.WebHost.UseUrls("https://localhost:5027/");
 
             // Agregar el contexto de la base de datos de AdventureWorks
             builder.Services.AdventureWorksDBContext();
@@ -19,7 +21,7 @@ namespace AdventureWorksAPI
             builder.Services.AddControllers(options =>
                 {
                     WriteLine("Formatos por omision:");
-                    foreach(IOutputFormatter formatter in options.OutputFormatters)
+                    foreach (IOutputFormatter formatter in options.OutputFormatters)
                     {
                         OutputFormatter? mediaFormatter = formatter as OutputFormatter;
                         if (mediaFormatter == null)
@@ -34,13 +36,22 @@ namespace AdventureWorksAPI
                     }
                 })
                 .AddXmlDataContractSerializerFormatters()
-                .AddXmlSerializerFormatters();
+                .AddXmlSerializerFormatters()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+            builder.Services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+
+            // Agregar Cross-Origin Resource Sharing (CORS)
+            builder.Services.AddCors();
+
+            // Agregar servicio de HEALTH (chequeo para ver si el API esta arriba o no
+            builder.Services.AddHealthChecks()
+                .AddDbContextCheck<AdventureWorksDB>();
 
             var app = builder.Build();
 
@@ -55,8 +66,17 @@ namespace AdventureWorksAPI
 
             app.UseAuthorization();
 
+            app.UseCors(configurePolicy: options =>
+            {
+                options.WithMethods("GET", "POST", "PUT", "DELETE");
+                options.WithOrigins("https://localhost:7260/");
+            });
 
             app.MapControllers();
+
+            app.UseHealthChecks(path: "/health");
+
+            app.UseMiddleware<SecurityHeader>();
 
             app.Run();
         }
---
+++ AdventureWorksAPI/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using AdventureWorksNS.Data;
+using System.Collections.Concurrent;
+
+namespace AdventureWorksAPI.Repositories
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private static ConcurrentDictionary<int, ProductCategory>? productCategoryCache;
+        //Comentario, puede usar Redis para un cache mas eficiente ==> Open Source
+        private AdventureWorksDB db;
+
+        public ProductCategoryRepository(AdventureWorksDB injectedDB)
+        {
+            db = injectedDB;
+            if (productCategoryCache is null)
+            {
+                productCategoryCache = new ConcurrentDictionary<int,ProductCategory>(
+                    db.ProductCategories.ToDictionary(c=> c.ProductCategoryId));
+            }
+        }
+
+        public async Task<ProductCategory> CreateAsync(ProductCategory c)
+        {
+            EntityEntry<ProductCategory> agregado = await db.ProductCategories.AddAsync(c);
+            int afectados = await db.SaveChangesAsync();
+            if (afectados == 1)
+            {
+                if (productCategoryCache is null) return c;
+                return productCategoryCache.AddOrUpdate(c.ProductCategoryId, c, UpdateCache);
+            }
+            return null!;
+        }
+
+        private ProductCategory UpdateCache(int id, ProductCategory c)
+        {
+            ProductCategory? viejo;
+            if (productCategoryCache is not null)
+            {
+                if (productCategoryCache.TryGetValue(id, out viejo))
+                {
+                    if (productCategoryCache.TryUpdate(id,c, viejo))
+                    {
+                        return c;
+                    }
+                }
+            }
+            return null!;
+        }
+
+        public Task<IEnumerable<ProductCategory>> RetrieveAllAsync()
+        {
+            return Task.FromResult(productCategoryCache is null ?
+                Enumerable.Empty<ProductCategory>() : productCategoryCache.Values);
+        }
+
+        public Task<ProductCategory?> RetrieveAsync(int id)
+        {
+            if (productCategoryCache is null) return null!;
+            productCategoryCache.TryGetValue(id, out ProductCategory? c);
+            return Task.FromResult(c);
+        }
+
+        public async Task<ProductCategory?> UpdateAsync(int id, ProductCategory c)
+        {
+            db.ProductCategories.Update(c);
+            int afectados = await db.SaveChangesAsync();
+            if (afectados == 1)
+            {
+                return UpdateCache(id, c);
+            }
+            return null;
+        }
+
+        public async Task<bool?> DeleteAsync(int id)
+        {
+            ProductCategory? c = db.ProductCategories.Find(id);
+            if (c is null) return false;
+            db.ProductCategories.Remove(c);
+            int afectados = await db.SaveChangesAsync();
+            if (afectados == 1)
+            {
+                if (productCategoryCache is null) return null;
+                return productCategoryCache.TryRemove(id, out c);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
